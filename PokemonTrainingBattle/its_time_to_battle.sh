a15d6346a78987135924e1358da7e7afe5327f8e
Ian <Iallheim@datto.com>
2022-07-27 11:57:51 -0400

    Updated bash command to have more interactions

--- PokemonTrainingBattle/its_time_to_battle.sh
+++ PokemonTrainingBattle/its_time_to_battle.sh
@@ -2,5 +2,20 @@
 echo "Its time to battle!"
 echo "How many battles would you like to run?"
 read -p "Number of Battles: " runs
+read -p "Run in headed?(Yes/No): " headed
 
-poetry run pytest --flake-finder --flake-runs=$runs tests/battle.py -vvvvvv
+if [ $headed == 'Yes' ]
+then
+    poetry run pytest --headed --flake-finder --flake-runs=$runs tests/battle.py -vvvvvv
+else
+    poetry run pytest --flake-finder --flake-runs=$runs tests/battle.py -vvvvvv
+fi
+
+read -p "Would you like to see the Stats for pokemon battles?(Yes/No): " outcome
+
+if [ $outcome == 'Yes' ]
+then
+  less 'battle_outcomes.json'
+else
+  echo "Have a good day!"
+fi
